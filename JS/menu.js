5d8a370cab044d513a22d63ddfcde9bc95268a4b
humastefan19 <stefan_avensis@yahoo.com>
2019-04-18 14:42:53 +0300

    Stefan Commit

---
+++ JS/menu.js
@@ -0,0 +1,14 @@
+document.write("<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\n" +
+    "  <a class=\"navbar-brand\" href=\"#\">Navbar</a>\n" +
+    "  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+    "    <span class=\"navbar-toggler-icon\"></span>\n" +
+    "  </button>\n" +
+    "  <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\n" +
+    "    <div class=\"navbar-nav\">\n" +
+    "      <a class=\"nav-item nav-link active\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\n" +
+    "      <a class=\"nav-item nav-link\" href=\"#\">Features</a>\n" +
+    "      <a class=\"nav-item nav-link\" href=\"#\">Pricing</a>\n" +
+    "      <a class=\"nav-item nav-link disabled\" href=\"#\">Disabled</a>\n" +
+    "    </div>\n" +
+    "  </div>\n" +
+    "</nav>")
--- home.php
+++ home.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: stefa
- * Date: 4/18/2019
- * Time: 2:29 PM
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <script src = "JS/menu.js"></script>
+</body>
+</html>
