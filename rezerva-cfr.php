1420ebbfcba4dbe18bff40d673a0af792b1aef54
clementlarisa <mihaela-larisa.clement@my.fmi.unibuc.ro>
2019-05-16 15:28:28 +0300

    Larisa Clement Commit

--- rezerva-cfr.php
+++ rezerva-cfr.php
@@ -6,7 +6,9 @@ if($_SESSION['logged_in']){
     <html>
     <head>
         <title>CFR</title>
+        <link rel="stylesheet" href="CSS/footer.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     </head>
     <body>
     <?php
