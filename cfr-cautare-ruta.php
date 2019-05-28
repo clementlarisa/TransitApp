6dfb6b825f519226e6c2bf0063fa1776802828f2
clementlarisa <mihaela-larisa.clement@my.fmi.unibuc.ro>
2019-05-29 01:15:26 +0300

    CFR Home + Cautare + Rezervare

--- cfr-cautare-ruta.php
+++ cfr-cautare-ruta.php
@@ -6,7 +6,6 @@ if ($_SESSION['logged_in']) {
     <html>
     <head>
         <title>CFR</title>
-        <link rel="stylesheet" href="CSS/footer.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
               crossorigin="anonymous">
@@ -14,6 +13,8 @@ if ($_SESSION['logged_in']) {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="CSS/footer.css">
+
     </head>
     <body>
     <?php
@@ -23,7 +24,7 @@ if ($_SESSION['logged_in']) {
     <?php } else { ?>
         <script src="JS/menu.js"></script>
     <?php } ?>
-    <div class="container bg-dark" style="height:100%;">
+    <div class="container bg-dark" style="height:100vh;">
         <table class="table table-dark table-striped table-hover">
             <tr>
                 <th>Numarul Liniei</th>
@@ -32,46 +33,38 @@ if ($_SESSION['logged_in']) {
                 <th>Statia de Sosire</th>
                 <th>Ora de Sosire</th>
             </tr>
-<!--        </table>-->
-        <?php
-        $db = mysqli_connect('localhost', 'root', '', 'transport');
-        //cautare statie cfr
-        $plecare = $_POST['cfr-cauta-plecare'];
-        $sosire = $_POST['cfr-cauta-destinatie'];
-        if (isset($plecare)) {
-//            $linie = "SELECT linie_id FROM transport.statie WHERE denumire_statie = '$plecare'";
-//            $show = "SELECT * FROM transport.statie WHERE linie_id = '$linie' AND denumire_statie = '$destinatie'";
-            $show = "SELECT P.linie_id AS linie_id, P.ora_plecare AS ora_plecare, S.ora_sosire AS ora_sosire,
+            <?php
+            $db = mysqli_connect('localhost', 'root', '', 'transport');
+            //cautare statie cfr
+            $plecare = $_POST['cfr-cauta-plecare'];
+            $sosire = $_POST['cfr-cauta-destinatie'];
+            if (isset($plecare)) {
+                $show = "SELECT P.linie_id AS linie_id, P.ora_plecare AS ora_plecare, S.ora_sosire AS ora_sosire,
                             P.denumire_statie AS statie_plecare, S.denumire_statie AS statie_sosire
-FROM transport.statie P 
-INNER JOIN transport.statie S 
-ON P.linie_id = S.linie_id
-WHERE p.denumire_statie ='$plecare' 
-AND S.denumire_statie = '$sosire'
-";
-            $result = mysqli_query($db, $show);
-            while ($rows = mysqli_fetch_array($result)) {
-                echo "<tr class=''>";
-                echo "<td>";
-                echo $rows['linie_id'];
-                echo "</td>";
-                echo "<td>";
-                echo $rows['statie_plecare'];
-                echo "</td>";
-                echo "<td>";
-                echo $rows['ora_plecare'];
-                echo "</td>";
-                echo "<td>";
-                echo $rows['statie_sosire'];
-                echo "</td>";
-                echo "<td>";
-                echo $rows['ora_sosire'];
-                echo "</td>";
-                echo "<br/>";
-                echo "</table>";
+                            FROM transport.statie P 
+                            INNER JOIN transport.statie S 
+                            ON P.linie_id = S.linie_id
+                            WHERE p.denumire_statie ='$plecare' 
+                            AND S.denumire_statie = '$sosire'
+                         ";
+                $result = mysqli_query($db, $show);
+                while ($rows = mysqli_fetch_array($result)) {
+                    echo "<tr class=''>";
+                    echo " <form action=\"cfr-config-bilet.php\" method=\"post\" class=\"form-inline justify-content-center\">";
+                    echo "<td><input type = 'hidden' readonly name = 'linie_id' value = " . $rows['linie_id'] . ">" . $rows['linie_id'] . "</td>";
+                    echo "<td><input type = 'hidden' readonly name = 'statie_plecare' value = " . $rows['statie_plecare'] . ">" . $rows['statie_plecare'] . "</td>";
+                    echo "<td><input type = 'hidden' readonly name = 'ora_plecare' value = " . $rows['ora_plecare'] . ">" . $rows['ora_plecare'] . "</td>";
+                    echo "<td><input type = 'hidden' readonly name = 'statie_sosire' value = " . $rows['statie_sosire'] . ">" . $rows['statie_sosire'] . "</td>";
+                    echo "<td><input type = 'hidden' readonly name = 'ora_sosire' value = " . $rows['ora_sosire'] . ">" . $rows['ora_sosire'] . "</td>";;
+                    echo "<td>";
+                    echo "<button class=\"btn btn-outline-success my-2 my-sm-0 btn-block\" type=\"submit\">Continua</button>";
+                    echo "</td>";
+                    echo "</form>";
+                    echo "<br/>";
+                }
             }
-        }
-        ?>
+            ?>
+        </table>
     </div>
     <?php
     if ($_SESSION['logged_in']) {
