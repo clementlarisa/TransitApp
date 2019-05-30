5bbbc6c88703af21a868f7e06b8996b3f608778e
Ligia-Andreea <LIGIA-ANDREEA.MERISAN@my.fmi.unibuc.ro>
2019-05-30 14:19:01 +0300

    commit linii+statii (rute) ligi

--- metrorex-statii.php
+++ metrorex-statii.php
@@ -32,7 +32,7 @@ if ($_SESSION['logged_in']) {
     <?php } ?>
 
     <div class="container bg-light" style="height:100vh;">
-        <table class="table table-light table-striped table-hover">
+        <table class="table table-dark table-striped table-hover" style = "color: rgb(51, 133, 255)">
             <tr>
                 <th>Numar statie</th>
                 <th>Numar linie</th>
